cea254733cb2a7fa20a982d250fdc74dda914855
Matthew Chan <mat_t4@hotmail.com>
2019-01-07 16:56:03 -0800

    Initial Commit

---
+++ Word_effects_part_1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word_effects_part_1
+//
+//  Created by Matthew Chan on 2019-01-07.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
