556c9988168bc67f9ce736b9935ec9a1b2f57aa8
Matthew Chan <mat_t4@hotmail.com>
2019-01-07 17:16:51 -0800

    Completed

--- Word_effects_part_1/main.m
+++ Word_effects_part_1/main.m
@@ -26,8 +26,7 @@ int main(int argc, const char * argv[]) {
     
     // print NSString object
     NSLog(@"Input was: %@", inputString);
-        
-        //this is a comment
+        NSLog(@"%p", inputString);
     }
     
 }
